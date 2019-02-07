3a66705bbe3ee7a675a441d5ae5c94225f7eeb05
Jasjeet Singh <jasjeet.s@applify.co>
2019-02-07 11:59:26 +0530

    #git_source

--- config/deploy.rb
+++ config/deploy.rb
@@ -41,6 +41,7 @@ end
 # All paths in `shared_dirs` and `shared_paths` will be created on their own.
 task :setup do
   # command %{rbenv install 2.3.0 --skip-existing}
+  command %{gem update bundler}
 end
 
 desc "Deploys the current version to the server."
